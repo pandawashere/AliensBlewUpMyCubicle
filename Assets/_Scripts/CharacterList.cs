93becb8b054b478748a24aeed945bf776d4a2ad7
pandawashere <sakura_mazz@hotmail.com>
2017-05-04 10:28:59 -0400

    initial commit

---
+++ Assets/_Scripts/CharacterList.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterList : MonoBehaviour {
+
+	public GameObject Jake;
+	public GameObject Henry;
+	public GameObject Jeff;
+	public GameObject Maria;
+	public GameObject Alien;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/_Scripts/DialogueData.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DialogueData : MonoBehaviour {
+	public Text _textComponent;
+
+	public string[] DialogueStrings;
+
+	public float SecondsBetweenCharacters = 0.5f;
+	public float CharacterRateMultiplier = 0.75f;
+
+	public KeyCode DialogueInput = KeyCode.Return;
+
+	private bool _isStringBeingRevealed = false;
+	private bool _isDialoguePlaying = false;
+	private bool _isEndOfDialogue = false;
+
+	public GameObject Choices;
+
+
+	// Use this for initialization
+	void Start () {
+		_textComponent = GetComponent<Text> ();
+		_textComponent.text = "";
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!_isEndOfDialogue) {
+			if (!_isDialoguePlaying) {
+				_isDialoguePlaying = true;
+				StartCoroutine (StartDialogue ());
+			}
+		}
+
+	}
+
+	private IEnumerator StartDialogue()
+	{
+		int dialogueLength = DialogueStrings.Length;
+		int currentDialogueIndex = 0;
+
+		while (currentDialogueIndex < dialogueLength || !_isStringBeingRevealed){
+			if (!_isStringBeingRevealed){
+				_isStringBeingRevealed = true;
+				StartCoroutine (DisplayString (DialogueStrings [currentDialogueIndex++]));
+				if (currentDialogueIndex >= dialogueLength){
+					_isEndOfDialogue = true;
+				}
+			}
+			yield return 0;
+		}
+			
+		_isDialoguePlaying = false;
+	}
+
+
+	private IEnumerator DisplayString(string stringToDisplay){
+		int stringLength = stringToDisplay.Length;
+		int currentCharacterIndex = 0;
+
+
+
+		_textComponent.text = "";
+
+		while (currentCharacterIndex < stringLength) {
+
+			_textComponent.text += stringToDisplay [currentCharacterIndex];
+			currentCharacterIndex++;
+
+			if (currentCharacterIndex < stringLength) {
+				if (Input.GetKey (DialogueInput)) {
+					yield return new WaitForSeconds (SecondsBetweenCharacters * CharacterRateMultiplier);
+				} else {
+
+					yield return new WaitForSeconds (SecondsBetweenCharacters);
+				}
+			} else {
+				break;
+			}
+		}
+		ShowIcon();
+
+		while (true) {
+			if (Input.GetKeyDown (DialogueInput)) {
+				break;
+			}
+			yield return 0;
+		}
+
+		_isStringBeingRevealed = false;
+		_textComponent.text = "";
+	}
+
+
+	private void ShowIcon(){
+		if (_isEndOfDialogue == true) {
+			Choices.SetActive (true);
+
+
+
+
+
+			return;
+		}
+
+	}
+}
