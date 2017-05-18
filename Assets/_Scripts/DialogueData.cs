b906879eea41180eba36f436b2375b22eca2ed7c
pandawashere <sakura_mazz@hotmail.com>
2017-05-18 02:46:24 -0400

    Cleaned up scripts

--- Assets/_Scripts/DialogueData.cs
+++ Assets/_Scripts/DialogueData.cs
@@ -8,17 +8,11 @@ public class DialogueData : MonoBehaviour {
 
 	public string[] DialogueStrings;
 
-	//private float SecondsBetweenCharacters = 0.01f;
-	//private float CharacterRateMultiplier = 0.001f;
-
 	public KeyCode DialogueInput = KeyCode.Mouse0;
 
-	//private bool _isStringBeingRevealed = false;
-	//private bool _isDialoguePlaying = false;
 	private bool _isEndOfDialogue = false;
 	private bool _isDialogueActive = true;
 	public GameObject Choices;
-
 
 	public GameObject char1;
 	public Sprite[] character1;
@@ -31,15 +25,11 @@ public class DialogueData : MonoBehaviour {
 
 	private int currentDialogueLine;
 
-	// Use this for initialization
 	void Start () {
 		_textComponent = GetComponent<Text> ();
 		_textComponent.text = "";
-
-
 	}
 
-	// Update is called once per frame
 	void Update ()
 	{
 		if (_isDialogueActive && currentDialogueLine < DialogueStrings.Length -1 && Input.GetKeyDown(KeyCode.Mouse0)) { 
@@ -50,9 +40,7 @@ public class DialogueData : MonoBehaviour {
 			_isEndOfDialogue = true;
 			ShowChoices ();
 			_isDialogueActive = false;
-
-
-		}
+			}
 		_textComponent.text = DialogueStrings [currentDialogueLine];
 		char1.GetComponent<SpriteRenderer>().sprite = character1[currentDialogueLine];
 		char2.GetComponent<SpriteRenderer>().sprite = character2[currentDialogueLine];
@@ -68,89 +56,4 @@ public class DialogueData : MonoBehaviour {
 	}
 }
 
-		//	char1.GetComponent<SpriteRenderer>().sprite = character1[currentDialogueIndex];
-
-
-
-
-
-
-
-
-
-	//{
-	//	if (!_isEndOfDialogue) {
-	//		if (!_isDialoguePlaying) {
-	//			_isDialoguePlaying = true;
-	//			StartCoroutine (StartDialogue ());
-	//		}
-	//	}
-
-	//}
-
-	//public IEnumerator StartDialogue()
-	//{
-	//	int dialogueLength = DialogueStrings.Length;
-	//	int currentDialogueIndex = 0;
-
-
-	//	while (currentDialogueIndex < dialogueLength || !_isStringBeingRevealed){
-	//		if (!_isStringBeingRevealed){
-	//			_isStringBeingRevealed = true;
-	//			StartCoroutine (DisplayString (DialogueStrings [currentDialogueIndex++]));
-	//			if (currentDialogueIndex >= dialogueLength){
-	//				_isEndOfDialogue = true;
-	//			}
-	//		}
-	//		yield return 0;
-	//	}
-			
-	//	_isDialoguePlaying = false;
-	//}
-		
-
-	//public IEnumerator DisplayString(string stringToDisplay){
-	//	int stringLength = stringToDisplay.Length;
-	//	int currentCharacterIndex = 0;
-	//
-	//	_textComponent.text = "";
-	//
-	//	while (currentCharacterIndex < stringLength) {
-	//
-	//			_textComponent.text += stringToDisplay [currentCharacterIndex];
-	//			currentCharacterIndex++;
-	//
-	//
-	//		if (currentCharacterIndex < stringLength) {
-	//			if (Input.GetKey (DialogueInput)) {
-	//				yield return new WaitForSeconds (SecondsBetweenCharacters * CharacterRateMultiplier);
-	//			} else {
-	//				yield return new WaitForSeconds (SecondsBetweenCharacters);
-	//			}
-	//		} else {
-	//			break;
-	//		}
-	//	}
-	//	ShowChoices();
-	//
-	//	while (true) {
-	//		if (Input.GetKeyDown (DialogueInput)) {
-	//			break;
-	//		}
-	//		yield return 0;
-	//	}
-	//
-	//	_isStringBeingRevealed = false;
-	//	_textComponent.text = "";
-	//}
-
-
-	//private void ShowChoices(){
-	//	if (_isEndOfDialogue == true) {
-	//		Choices.SetActive (true);
-	//
-	//		return;
-	//	}
-
-	//}
-//}
+	
